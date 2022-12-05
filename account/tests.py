34e58220a79bd6f233cb48784722ec9c1b7fedf3
aanastasiyatuz <aanastasiyatuz@gmail.com>
2022-12-05 18:10:24 +0600

    test

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,16 @@
+import requests
 from django.test import TestCase
 
-# Create your tests here.
+class CheckAuthTest(TestCase):
+    def test_login(self):
+        su_response = requests.post("http://127.0.0.1:8000/account/login/", {"email":"admin@admin.com", "password":"1"})
+        u_response = requests.post("http://127.0.0.1:8000/account/login/", {"email":"test@gmail.com", "password":"string"})
+
+        self.assertEqual(su_response.status_code, 200)
+        self.assertEqual(u_response.status_code, 200)
+
+        self.assertIn("access", su_response.json())
+        self.assertIn("refresh", su_response.json())
+
+        self.assertIn("access", u_response.json())
+        self.assertIn("refresh", u_response.json())
